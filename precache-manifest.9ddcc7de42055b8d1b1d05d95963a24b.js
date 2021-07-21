2a7831a11fa7c702b816b5af1fd473b4c09a9b2b
Dylan Neil <dylanneil21@gmail.com>
2021-07-21 13:47:06 -0500

    Updates

---
+++ precache-manifest.9ddcc7de42055b8d1b1d05d95963a24b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249b51b297bf7120702277dae5ed765d",
+    "url": "/neilarreola.github.io/medic-app/index.html"
+  },
+  {
+    "revision": "590d6cec26aabbac03c6",
+    "url": "/neilarreola.github.io/medic-app/static/css/2.0422e928.chunk.css"
+  },
+  {
+    "revision": "7a39f814f49408678817",
+    "url": "/neilarreola.github.io/medic-app/static/css/main.685b6b2e.chunk.css"
+  },
+  {
+    "revision": "590d6cec26aabbac03c6",
+    "url": "/neilarreola.github.io/medic-app/static/js/2.fbd872ad.chunk.js"
+  },
+  {
+    "revision": "0191283e9d9a71d9f08da7e76b831461",
+    "url": "/neilarreola.github.io/medic-app/static/js/2.fbd872ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f301b70dbadab7c91ec1",
+    "url": "/neilarreola.github.io/medic-app/static/js/3.a98e5f45.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/neilarreola.github.io/medic-app/static/js/3.a98e5f45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "268c5b5510e49c80fae3",
+    "url": "/neilarreola.github.io/medic-app/static/js/4.65f11dfc.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/neilarreola.github.io/medic-app/static/js/4.65f11dfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80dc7dd91a2185c969a1",
+    "url": "/neilarreola.github.io/medic-app/static/js/5.a3309bac.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/neilarreola.github.io/medic-app/static/js/5.a3309bac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a39f814f49408678817",
+    "url": "/neilarreola.github.io/medic-app/static/js/main.1c27f898.chunk.js"
+  },
+  {
+    "revision": "85f7a8429670648e5fb3",
+    "url": "/neilarreola.github.io/medic-app/static/js/runtime-main.81ff7a5b.js"
+  },
+  {
+    "revision": "b9d95e9f5d63f5eca1d4d8a719dbd817",
+    "url": "/neilarreola.github.io/medic-app/static/media/wave.b9d95e9f.svg"
+  }
+]);
--- src/components/alerts/Alert.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Alert = ({texto}) => {
-  return ( 
-    <div className="alert alert-warning text-white text-center">{texto}</div>
-   );
-}
- 
-export default Alert;
